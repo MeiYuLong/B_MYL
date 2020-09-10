941b65cefc8d26e83c302ec71e6c50b959b44449
YuLong Mei <longjiao914@126.com>
2020-09-10 20:23:03 +0800

    0.0.1 初始化控件B

---
+++ B_MYL/Source/DemoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DemoViewController.swift
+//
+//  Created by casa on 2020/2/21.
+//  Copyright © 2020 casa. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        
+        self.navigationItem.title = "组件B-DemoViewController"
+    }
+}
